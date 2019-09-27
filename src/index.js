c8fcb91aad0da94bbcdff99a280dbabb957d811a
Jarod Burchill <jarodburchill@gmail.com>
2019-09-27 08:24:23 -0400

    Added Navbar and more config

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import Navbar from "./components/Navbar";
+export default Navbar;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.jsx",
+  entry: "./src/index.js",
   output: {
     path: path.resolve("lib"),
     filename: "index.js",
